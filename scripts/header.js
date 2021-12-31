a955eb855e873d4ac7752af3f360cbf939d7f4ea
Crisnn <cristovao.neto@dilettasolutions.com>
2021-12-30 21:27:34 -0300

    feat: Add Header scrolling animation

---
+++ scripts/header.js
@@ -0,0 +1,22 @@
+const headerUI = document.querySelector('#page-header');
+const oneBlincLogoUI = document.querySelector('#oneblinc-logo');
+
+let scrolled = false;
+
+document.addEventListener('scroll', e => {
+  if (window.scrollY > 120) {
+    if (!scrolled) {
+      scrolled = true;
+      headerUI.classList.remove('top-page');
+      headerUI.classList.add('sticky');
+      oneBlincLogoUI.src = 'assets/pngs/oneblinc-logo-light.png';
+    }
+  } else {
+    if (scrolled) {
+      scrolled = false;
+      headerUI.classList.add('top-page');
+      headerUI.classList.remove('sticky');
+      oneBlincLogoUI.src = 'assets/pngs/oneblinc-logo-dark.png';
+    }
+  }
+});
